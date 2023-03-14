4b72f9cfb6298612ac36f9ae89ffdf6c9ce86051
jongukchoi95 <jongukchoi95@gmail.com>
2023-03-14 17:30:06 +0900

    FEAT : UserController PostMapping 추가

--- backend/SpringFramework/bookdodum/src/main/java/com/sasatech/bookdodum/controller/UserController.java
+++ backend/SpringFramework/bookdodum/src/main/java/com/sasatech/bookdodum/controller/UserController.java
@@ -6,6 +6,7 @@ import com.sasatech.bookdodum.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class UserController {
 
     private final UserService userService;
 
+    @PostMapping("/")
     public ResponseEntity<?> addUser(@RequestBody UserRequestDto userRequestDto) {
         userService.addUser(userRequestDto);
         return new ResponseEntity(new ApiResponseDto(true, "addUser Success", null), HttpStatus.OK);
