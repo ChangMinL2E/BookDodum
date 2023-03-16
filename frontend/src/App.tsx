6784fc9d43946fd16a3c5bf26ab948f77037a369
choizlor <choizlor@gmail.com>
2023-03-16 17:05:16 +0900

    FEAT: 바코드 사진 test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Reader from "./pages/Reader.js";
 import Home from "./pages/Home";
+import {ReaderTest} from "./pages/ReaderTest";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/reader" element={<Reader />} />
+        <Route path="/readertest" element={<ReaderTest />} />
       </Routes>
     </BrowserRouter>
   );
