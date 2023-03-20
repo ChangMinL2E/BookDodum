cc2d99023e32a6743b1fd2f0951f7f255fb0c49c
choizlor <choizlor@gmail.com>
2023-03-20 22:39:42 +0900

    FEAT: 독서모임 카드 css

--- frontend/src/pages/Group/index.tsx
+++ frontend/src/pages/Group/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import GroupCover from "../../Components/Contents/GroupCover";
 import styled from "styled-components";
 import sample from "../../Assets/Images/sample.png";
+import Nav from "../../Components/Common/Nav";
 
 interface BookGroup {
   writer: string;
@@ -19,32 +20,46 @@ export default function Group() {
 
   return (
     <Container>
+      <Nav />
       <Text>참여중인 독서 모임</Text>
-      <GroupCover
-        imageUrl={sample}
-        title={title}
-        author={author}
-        participant={participant}
-      />
-      <GroupCover
-        imageUrl={sample}
-        title={title}
-        author={author}
-        participant={participant}
-      />
+      <BookGroupCards>
+        <GroupCover
+          imageUrl={sample}
+          title={title}
+          author={author}
+          participant={participant}
+        />
+        <GroupCover
+          imageUrl={sample}
+          title={title}
+          author={author}
+          participant={participant}
+        />
+      </BookGroupCards>
     </Container>
   );
 }
 
+// Styled Components
 const Container = styled.div`
+  background-color: #f5eede;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const BookGroupCards = styled.div`
   display: flex;
-  overflow: scroll;
+  overflow-x: scroll;
   min-height: 190px;
   padding-left: 3%;
 `;
 
 const Text = styled.div`
+  // 상우하좌
+  margin: 20px 0px 15px 20px;
   color: #5c5649;
   text-shadow: 0px 1px 2px rgba(0, 0, 0, 0.25);
   font-weight: 700;
+  font-size: 1.1rem;
+  font-weight: 600;
 `;
