83dc10fb74527149a9e0a0ddeba7057149fea529
choizlor <choizlor@gmail.com>
2023-03-22 15:53:50 +0900

    FEAT: login/signup 컴포넌트 생성

---
+++ frontend/src/pages/Signup/Index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Signup() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+const Container = styled.div`
+    background-color: #5C5649;
+`
+
