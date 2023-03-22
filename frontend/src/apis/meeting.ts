5b7ad57f6d50b0884cc8ffd82ca9ee3ce911a195
choizlor <choizlor@gmail.com>
2023-03-22 17:41:16 +0900

    FEAT: 독서모임 axios 생성

--- frontend/src/apis/meeting.ts
+++ frontend/src/apis/meeting.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from "axios";
 
 const baseUrl = process.env.REACT_APP_API_URL;
 
@@ -9,20 +9,50 @@ interface Meeting {
   authority: boolean;
 }
 
+interface Comment {
+  meetingId: number;
+  content: string;
+}
+
+// 모임 만들기
 export async function createMeeting(meeting: Meeting) {
-    try {
-      const { data } = await axios({
-        method: "POST",
-        url: `${baseUrl}/book/meeting`,
-        data: {
-          "bookId": meeting.bookId,
-          "title": meeting.title,
-          "content": meeting.content,
-          "authority": meeting.authority,
-        }
-      });
-      return data;
-    } catch (e) {
-      console.log(e);
-    }
+  try {
+    const { data } = await axios({
+      method: "POST",
+      url: `${baseUrl}/book/meeting`,
+      data: meeting,
+    });
+    return data;
+  } catch (e) {
+    console.log(e);
   }
+}
+
+// 모임 조회하기
+export async function postMeeting() {
+  try {
+    const { data } = await axios({
+      method: "GET",
+      url: `${baseUrl}/book/meeting`,
+    });
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+// 모임 댓글 쓰기
+export async function postMeetingComment(comment: Comment) {
+  try {
+    const { data } = await axios({
+      method: "POST",
+      url: `${baseUrl}/book/meeting/comment`,
+      data: comment,
+    });
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+// 모임 댓글 조회하기
