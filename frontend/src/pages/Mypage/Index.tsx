6670ee7e68a9a7c205342eca1e8acac5327fa74c
나혜승 <nahs00122@gmail.com>
2023-03-30 11:18:54 +0900

    FEAT: DELETE AXIOS 추가

--- frontend/src/pages/Mypage/Index.tsx
+++ frontend/src/pages/Mypage/Index.tsx
@@ -6,7 +6,6 @@ import Reading from "./Reading";
 import useSelectorTyped from "../../Store";
 
 export default function Mypage() {
-  const nickname = useSelectorTyped((state) => state.user.name);
   return (
     <Container>
       <MypageBanner />
@@ -20,11 +19,4 @@ export default function Mypage() {
 const Container = styled.div`
   background-color: #5c5649;
   height: 100vh;
-`;
-
-const ReadText = styled.div`
-  color: #f9f9f7;
-  padding-top: 3%;
-  margin-left: 5%;
-  size: 13px;
 `;
