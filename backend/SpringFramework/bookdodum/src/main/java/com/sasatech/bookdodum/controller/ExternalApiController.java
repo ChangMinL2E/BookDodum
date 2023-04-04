03b04a351d78cf07a430dead4c43483cc06272a8
wonseok <bulnabang99@gmail.com>
2023-04-04 17:15:39 +0900

    Feat: 외부 API 요청 swagger 설명(Operation) 추가
    Former-commit-id: 04c615f48a6f71f055e6fc8591858ede66fc9be8

--- backend/SpringFramework/bookdodum/src/main/java/com/sasatech/bookdodum/controller/ExternalApiController.java
+++ backend/SpringFramework/bookdodum/src/main/java/com/sasatech/bookdodum/controller/ExternalApiController.java
@@ -2,6 +2,7 @@ package com.sasatech.bookdodum.controller;
 
 
 import com.sasatech.bookdodum.service.api.ExternalApiService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -39,6 +40,7 @@ public class ExternalApiController {
 
     // 이달의 키워드 조회
     @GetMapping("/bestkeyword")
+    @Operation(summary = "이달의 키워드 조회")
     public ResponseEntity<?> getBestKeywordAPI(@PathVariable("year") String year,
                                                @PathVariable("month") String month){
         return externalApiService.getBestKeywordAPI(year, month);
@@ -46,6 +48,7 @@ public class ExternalApiController {
 
     // 도서 소장 도서관 정보 불러오기
     @GetMapping("/library")
+    @Operation(summary = "도서 소장 도서관 정보 불러오기")
     public ResponseEntity<?> getLibraryAPI(@RequestParam("isbn") String isbn,
                                            @RequestParam("regioncode") String regionCode){
         return externalApiService.getLibraryAPI(isbn, regionCode);
@@ -53,6 +56,7 @@ public class ExternalApiController {
 
     // 도서관별 장서/대출 데이터 조회
     @GetMapping("/itemsrch")
+    @Operation(summary = "도서관별 장서/대출 데이터 조회")
     public ResponseEntity<?> getItemSrchAPI(@RequestParam("isbn") String isbn,
                                             @RequestParam("libcode") String libCode){
         return externalApiService.getItemSrchAPI(isbn, libCode);
@@ -60,18 +64,23 @@ public class ExternalApiController {
 
     // 도서관별 도서 소장여부 및 대출 가능여부 조회
     @GetMapping("/bookexist")
+    @Operation(summary = "도서관별 도서 소장여부 및 대출 가능여부 조회")
     public ResponseEntity<?> getBookExistAPI(@RequestParam("isbn") String isbn,
                                              @RequestParam("libcode") String libCode){
         return externalApiService.getBookExistAPI(isbn, libCode);
     }
 
+    // 현재 좌표를 기준으로 지역코드 불러오기
     @GetMapping("/regioncode")
+    @Operation(summary = "현재 좌표를 기준으로 지역코드 불러오기")
     public ResponseEntity<?> getRegionCodeAPI(@RequestParam("longitude") String longitude,
                                               @RequestParam("latitude") String latitude){
         return externalApiService.getRegionCodeAPI(longitude, latitude);
     }
 
+    // 내 지역 도서관 인기 대출 도서 목록 조회
     @GetMapping("/librarybooks")
+    @Operation(summary = "내 지역 도서관 인기 대출 도서 목록 조회")
     public ResponseEntity<?> getLibraryBooksAPI(@RequestParam("regioncode") String regionCode) {
 
         return externalApiService.getLibraryBooksAPI(regionCode);
