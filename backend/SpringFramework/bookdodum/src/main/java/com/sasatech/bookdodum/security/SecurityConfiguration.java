8ff092cf902c992d039a8a5efa83de810fcd40c1
wonseok970 <bulnabang99@gmail.com>
2023-04-06 02:36:45 +0900

    Etc:
    Former-commit-id: c0723f77edae5952462c9a38a107c850a0486a59

--- backend/SpringFramework/bookdodum/src/main/java/com/sasatech/bookdodum/security/SecurityConfiguration.java
+++ backend/SpringFramework/bookdodum/src/main/java/com/sasatech/bookdodum/security/SecurityConfiguration.java
@@ -17,31 +17,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//
-//        http.httpBasic().disable()
-//
-//                .csrf().disable()
-//
-//                .sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//
-//                .and()
-//                .authorizeRequests()
-//                .antMatchers("/user/signin","/user/signup").permitAll()
-//                .and()
-//                .exceptionHandling().accessDeniedHandler(new CustomAcessDeniedHandler())
-//                .and()
-//                .exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint())
-//
-//                .and()
-//                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),UsernamePasswordAuthenticationFilter.class);
-//
-//
-//        //super.configure(http);
-//    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
@@ -54,9 +29,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
                 .and()
                 .authorizeRequests()
-                .antMatchers("/user/signin","/user/signup").permitAll()
-//                .antMatchers("/api/public/**").permitAll()
-                .antMatchers("/api/**").authenticated() // /api 요청에 대해 인증 필요
+                .antMatchers("/user/signin","/user/signup", "/api/**", "/api/public/**").permitAll()
                 .and()
                 .exceptionHandling().accessDeniedHandler(new CustomAcessDeniedHandler())
                 .and()
@@ -71,9 +44,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     // http://43.201.102.210:8080/api/swagger-ui/index.html?docExpansion=none&operationsSorter=alpha&tagsSorter=alpha&url=/api/
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/v2/api-docs", "/swagger-ui.html", "/swagger-resources/**", "/webjars/**", "/swagger/**", "sign-api/exception");
+    @Override/  public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs", "/swagger-ui/**","/index.html", "/webjars/**","/swagger/**", "sign-api/exception");
+//        super.configure(web);
     }
 }
 
