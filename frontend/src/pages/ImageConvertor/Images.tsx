a54531cc689362a75a6de365bf2d8b7ba62f5545
나혜승 <nahs00122@gmail.com>
2023-04-06 10:32:51 +0900

    FIX: navigate 추가
    Former-commit-id: 49ee2ec14faf9c26fb6c4734b5f62927fc717a2f

--- frontend/src/pages/ImageConvertor/Images.tsx
+++ frontend/src/pages/ImageConvertor/Images.tsx
@@ -66,7 +66,7 @@ export default function Images({ imageUrls }: Props) {
         <Button
           onClick={() => {
             submitImage();
-            navigate(`reading/${bookId}`)
+            navigate("/mypage")
           }}
         >
           확인
